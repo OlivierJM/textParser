d7f6f94d0cafdf1ea5ffd6d6a19031d952d73716
Olivier JM <manolivier93@gmail.com>
2019-01-30 17:44:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function parseText(text) {
+    let filterSpace = item => item.length > 1;
+    let spacedWords = text
+      .split(" ")
+      .filter(filterSpace)
+      .map(word => `${word} `);
+  
+    spacedWords.map((word, i) => {
+      if (!word.indexOf("_")) {
+        let newWord = <span style={{ color: "blue" }}>{word.substring(1)}</span>;
+        spacedWords[i] = newWord;
+      }
+      if (!word.indexOf("*")) {
+        let starredWord = (
+          <span style={{ color: "red" }}>{word.substring(1)}</span>
+        );
+        spacedWords[i] = starredWord;
+      }
+    });
+    
+    return spacedWords;
+  }
+
+  module.exports = parseText
+  
